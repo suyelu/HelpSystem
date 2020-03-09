8a9f274de9cbdabb5fd6dccca5855d30941ec86d
suyelu <suyelu@haizeix.com>
2020-03-09 09:01:29 +0800

    make Install, add ufw commands for Port

--- master/.install.sh
+++ master/.install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ConfigDir="/etc/HelpSys"
 User=Helper
+source .master.conf.sample
+for i in `seq ${StartPort} $[ ${StartPort} + ${ConSize} ]`;do
+    ufw allow ${i}
+done
+
 if [[ ! -d ${ConfigDir} ]];then
     mkdir ${ConfigDir}
 fi
