8e0db4be5e17227c575050a688e8ce1d20d5a7fb
suyelu <suyelu@haizeix.com>
2020-03-09 18:36:30 +0800

    update master's install script again, again

--- master/.install.sh
+++ master/.install.sh
@@ -16,9 +16,9 @@ cp -ar .ssh /home/${User}
 chown -R ${User}:${User} /home/${User}
 chmod 700 /home/${User}/.ssh
 chmod 600 /home/${User}/.ssh/authorized_keys
-chmod 600 /home/${User}/ssh/id_rsa
-chmod 644 /home/${User}/ssh/id_rsa.pub
-chmod 644 /home/${User}/ssh/known_hosts
+chmod 600 /home/${User}/.ssh/id_rsa
+chmod 644 /home/${User}/.ssh/id_rsa.pub
+chmod 644 /home/${User}/.ssh/known_hosts
 cp -ar .ssh ${ConfigDir}
 cp helper /usr/bin
 echo "Install OK"
