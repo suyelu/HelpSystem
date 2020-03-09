441a2dc901350cd9a4c218745b16051d5d8123e9
suyelu <suyelu@haizeix.com>
2020-03-09 19:00:13 +0800

    add #define _DEBUG in common.h

--- common/common.h
+++ common/common.h
@@ -1,6 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
-
+#define _DEBUG
 
 #include "head.h"
 
@@ -32,30 +32,6 @@ int get_conf_value(char *pathname, const char* key_name, char *value);
 
 int socket_create(int port);
 
-int socket_accept(int sock_listen);
-
-int socket_connect(int port, char *host);
-
 int connect_nonblock(int port, char *host, long timeout);
-
-int connect_sock(struct sockaddr_in addr);
-
-bool check_connect(struct sockaddr_in addr, long timeout);
-
-int recv_data(int sockfd, char* buf, int bufsize);
-
-int send_response(int sockfd, int req); //
-
-int recv_response(int sockfd); //
-
-int generate_logname(int code, char *logname, char *logdir);
-
-int check_size(char *filename, int size, char *dir);
-
-//文件大于size M 的时候做压缩备份
-
-int write_Pi_log (char *PiHealthLog, const char *format, ...);
-
-
 
 #endif
