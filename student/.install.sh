7f4d6d25cf1aa5bb8282b064ec3b9d7c14aaea3e
Ginakira <ginakira@foxmail.com>
2020-03-10 10:19:22 +0800

    MacOS student.conf bugFix

--- student/.install.sh
+++ student/.install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 ConfigDir="/etc/HelpSys"
+check_system_info=`uname`
 
 if [[ ! -d ${ConfigDir} ]]; then
     mkdir ${ConfigDir}
 fi
 read -p "请输入你的真实姓名:" Name
 cp ./.student.conf.sample ${ConfigDir}/student.conf
-sed -i 's/XXX/'''${Name}'''/g' ${ConfigDir}/student.conf
+
+if [[ $check_system_info =~ "Darwin" ]]; then
+    sed -i '' 's/XXX/'''${Name}'''/g' ${ConfigDir}/student.conf
+else
+    sed -i 's/XXX/'''${Name}'''/g' ${ConfigDir}/student.conf
+fi
+
 cp helpme /usr/bin || cp helpme /usr/local/bin
 echo "Install OK"
 echo "You can change you config in ${ConfigDir}/student.conf."
