097054aed790fd50fe160f323e31a9ebfbf485ad
suyelu <suyelu@126.com>
2022-04-20 12:37:07 +0800

    fixed bug for deleting other keys

--- student/check_key.sh
+++ student/check_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 key_file=$1
 
-Msg=`cat ${key_file} | cut -d " " -f 2 | cut -c 10-20`
+Msg=`cat ${key_file} | cut -d " " -f 2 | cut -c 1-100`
 sed -i -e '/'''${Msg}'''/d' ~/.ssh/authorized_keys
 cat  ${key_file} >> ~/.ssh/authorized_keys 
