a895f5a3a1f5fc097570343f9456fd2bb2780f0e
suyelu <suyelu@126.com>
2022-05-11 12:05:22 +0800

    Update check_key.sh

--- student/check_key.sh
+++ student/check_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 key_file=$1
 
-Msg=`cat ${key_file} | cut -d " " -f 2 | cut -c 1-100`
+Msg=`cat ${key_file} | cut -d " " -f 2 | cut -c 1-80`
 sed -i -e '/'''${Msg}'''/d' ~/.ssh/authorized_keys
 cat  ${key_file} >> ~/.ssh/authorized_keys 
